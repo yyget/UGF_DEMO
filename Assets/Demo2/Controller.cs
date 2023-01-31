2757dc1a365e55a576f9ec7c105fb53de859b9d9
zyy <286362118@qq.com>
2023-01-31 15:01:31 +0800

    卸载场景

--- Assets/Demo2/Controller.cs
+++ Assets/Demo2/Controller.cs
@@ -7,6 +7,15 @@ public class Controller : MonoBehaviour
     public void EnterGame()
     {
         SceneComponent Scene =  GameEntry.GetComponent<SceneComponent>();
+
+        // 卸载所有场景
+        string[] loadedSceneAssetNames = Scene.GetLoadedSceneAssetNames();
+        for (int i = 0; i < loadedSceneAssetNames.Length; i++)
+        {
+            Scene.UnloadScene(loadedSceneAssetNames[i]);
+        }
+
+        // 加载游戏场景
         Scene.LoadScene("Assets/Demo2/Demo2Game.unity", this);
     }
 }
