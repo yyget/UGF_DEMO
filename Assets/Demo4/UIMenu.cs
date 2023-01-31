79f71cca565c31b95adee8e72e96a0357cfe98cb
zyy <286362118@qq.com>
2023-01-31 18:00:24 +0800

    demo4 事件

--- Assets/Demo4/UIMenu.cs
+++ Assets/Demo4/UIMenu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityGameFramework.Runtime;
-namespace Demo3
+namespace Demo4
 {
     public class UIMenu : UIFormLogic
     {
